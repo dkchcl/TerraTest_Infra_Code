0ef7bed3328576e0ac59a731ec570fe5b44609c2
dkchcl <dkc.hcl@gmail.com>
2025-12-26 21:14:39 +0530

    add code

--- Enviroments/Dev/provider.tf
+++ Enviroments/Dev/provider.tf
@@ -5,11 +5,15 @@ terraform {
       version = "4.47.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "dkc-state-rg"
+    storage_account_name = "dkcstatestg1234"
+    container_name       = "statefiles"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  features {
-    
-  }
+  features {}
   subscription_id = "009fad33-c09c-4841-af38-57dd79870d40"
 }
